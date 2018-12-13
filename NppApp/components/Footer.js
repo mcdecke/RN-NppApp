0c52380c26db48aac96c7bb794115331bc014c3f
Mac Decker <mcdecke@gmail.com>
2018-12-13 11:41:12 -0700

    modular

--- NppApp/components/Footer.js
+++ NppApp/components/Footer.js
@@ -3,7 +3,10 @@ import {
   StyleSheet,
   Text,
   View,
-  Dimensions
+  Dimensions,
+  Modal,
+  TouchableHighlight,
+  Alert
 } from 'react-native';
 import {Icon, Button, Card} from 'react-native-elements'
 
@@ -11,9 +14,40 @@ import {Icon, Button, Card} from 'react-native-elements'
 const SCREEN_WIDTH = Dimensions.get('window').width
 
 class Footer extends Component {
+
+  state = {
+    modalVisible: false
+  }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
   render(){
     return (
     <View style={styles.footerStyle}>
+    
+    <Modal
+        animationType="slide"
+        transparent={false}
+        visible={this.state.modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+        }}>
+        <View style={{marginTop: 22}}>
+          <View>
+            <Text>Hello World!</Text>
+
+            <TouchableHighlight
+              onPress={() => {
+                this.setModalVisible(!this.state.modalVisible);
+              }}>
+              <Text>Hide Modal</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
       <Icon
         raised
         style={styles.iconStyle}
@@ -26,15 +60,13 @@ class Footer extends Component {
         <Text>{this.props.index + 1}/{this.props.posters.length}</Text>
       </Card>
       <Icon
-
         raised
         style={styles.iconStyle}
         name='info-circle'
         type='font-awesome'
         color='#f50'
-        onPress={() => console.log('hello')}
+        onPress={() => this.setModalVisible(true)}
       />
-
     </View>
     )
   }
